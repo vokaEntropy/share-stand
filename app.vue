5ef4066974e30606a7b62c7172bd7ee8cf2899bf
vokaEntropy <60666903+vokaEntropy@users.noreply.github.com>
2023-12-19 16:54:35 +0300

    first com

---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <button @click="mobileShare">Share msg</button>
+  </div>
+</template>
+
+<script setup>
+const mobileShare = async () => {
+  if (process.client) {
+    const image = 'https://thefastest.megafon.ru/images/og/dragon.jpg'
+
+    const text = `'Поймай дракона, получи предсказание'\n\n#МегаФон2024\nhttps://thefastest.megafon.ru`;
+
+    try {
+      await navigator.share({
+        title: "Поймай дракона, получи предсказание",
+        text,
+      });
+    } catch (err) {
+      console.error("Share failed:", err);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
